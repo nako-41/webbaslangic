15ad530249fa9e0512e8d026b7b7be104e0886f6
okan kaipoglu <kaipoglu267@gmail.com>
2021-11-29 11:25:59 +0300

    geliştirme

---
+++ script.js
@@ -0,0 +1,12 @@
+// alert("merhaba dünya")
+// console.log("merhaba dünya")
+
+var person = {
+   name="okan",
+   age=24
+
+}
+console.log(person.age);
+console.log(typeof person);
+
+alert(person);
